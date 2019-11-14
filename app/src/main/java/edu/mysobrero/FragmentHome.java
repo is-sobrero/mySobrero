b3ee14a84d10895353728d8085e730d08f2eebad
Federico Runco <federico.runco@gmail.com>
2019-11-14 21:41:17 +0100

    Modifiche alla UI

--- app/src/main/java/edu/mysobrero/FragmentHome.java
+++ app/src/main/java/edu/mysobrero/FragmentHome.java
@@ -1,23 +1,31 @@
 package edu.mysobrero;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.Fragment;
 
 import com.squareup.picasso.Picasso;
 
-public class FragmentHome extends Fragment {
+import saschpe.android.customtabs.CustomTabsHelper;
+import saschpe.android.customtabs.WebViewFallback;
+
+public class FragmentHome extends Fragment implements View.OnClickListener{
     REAPIResponse response;
     FeedClass feed;
 
     TextView welcome, info, mittComm, dataComm, comm, art1Titolo, art2Titolo, art3Titolo, lastVoto, lastVotoDesc, compitiCont;
     ImageView art1Img, art2Img, art3Img;
+
+    Button not1, not2, not3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +34,24 @@ public class FragmentHome extends Fragment {
         this.response = mainActivity.response;
         this.feed = mainActivity.feed;
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String url = "";
+        if (v == not1) url = this.feed.articoli.get(0).link;
+        if (v == not2) url = this.feed.articoli.get(1).link;
+        if (v == not3) url = this.feed.articoli.get(2).link;
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
+                .addDefaultShareMenuItem()
+                .setShowTitle(true)
+                .build();
+
+        CustomTabsHelper.addKeepAliveExtra(getContext(), customTabsIntent.intent);
+
+        CustomTabsHelper.openCustomTab(getContext(), customTabsIntent,
+                Uri.parse(url),
+                new WebViewFallback());
     }
 
     @Override
@@ -43,6 +69,9 @@ public class FragmentHome extends Fragment {
         compitiCont = getView().findViewById(R.id.compitiCount);
         mittComm = getView().findViewById(R.id.mittenteComm);
         comm = getView().findViewById(R.id.testoComm);
+        not1 = getView().findViewById(R.id.art1_btn);
+        not2 = getView().findViewById(R.id.art2_btn);
+        not3 = getView().findViewById(R.id.art3_btn);
 
         welcome.setText("Ciao " + this.response.user.nome + "!");
         info.setText(String.format("Classe %s %s - %s", this.response.user.classe, this.response.user.sezione, this.response.user.corso));
@@ -61,6 +90,8 @@ public class FragmentHome extends Fragment {
         art3Titolo.setText(this.feed.articoli.get(2).titolo);
         Picasso.get().load(this.feed.articoli.get(2).urlImmagine).into(art3Img);
 
-
+        not1.setOnClickListener(this);
+        not2.setOnClickListener(this);
+        not3.setOnClickListener(this);
     }
 }
