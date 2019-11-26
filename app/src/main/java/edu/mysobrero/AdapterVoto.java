6b3c07075ab1ec44869fcaca7c94b19a45ccafab
Federico Runco <federico.runco@gmail.com>
2019-11-27 00:03:35 +0100

    Grafico voti e filtraggio materie aggiunto

--- app/src/main/java/edu/mysobrero/AdapterVoto.java
+++ app/src/main/java/edu/mysobrero/AdapterVoto.java
@@ -1,31 +1,35 @@
 package edu.mysobrero;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AdapterVoto extends ArrayAdapter<REAPIResponse.Voti> {
+public class AdapterVoto extends ArrayAdapter<REAPIResponse.Voti> implements Filterable {
     public AdapterVoto(Context context, int textViewResourceId,
                        List<REAPIResponse.Voti> objects, EventListener listener) {
         super(context, textViewResourceId, objects);
         this.listener = listener;
+        this.voti = objects;
+        this.votiOriginali = objects;
     }
-
-    EventListener listener;
+    List<REAPIResponse.Voti> voti;
+    List<REAPIResponse.Voti> votiOriginali;
+    public EventListener listener;
 
     public interface EventListener {
-        void onEvent();
+        void onEvent(List<REAPIResponse.Voti> votiUpdated);
     }
-
-
-
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -47,15 +51,59 @@ public class AdapterVoto extends ArrayAdapter<REAPIResponse.Voti> {
                 } else {
                     expandView.setVisibility(View.GONE);
                 }
-                listener.onEvent();
             }
         });
-        REAPIResponse.Voti c = getItem(position);
+        REAPIResponse.Voti c = voti.get(position);
         voto.setText(c.voto);
         materia.setText(c.materia);
         tipo.setText(c.tipologia);
         data.setText(c.data);
         if (c.commento.length() > 0) commento.setText(c.commento);
         return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return voti.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                voti = (List<REAPIResponse.Voti>) results.values;
+                listener.onEvent(voti);
+                notifyDataSetChanged();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<REAPIResponse.Voti> FilteredArrayNames = new ArrayList<>();
+                constraint = constraint.toString().toLowerCase();
+                for (int i = 0; i < votiOriginali.size(); i++) {
+                    String dataNames = votiOriginali.get(i).materia;
+                    if (dataNames.toLowerCase().startsWith(constraint.toString()))  {
+                        FilteredArrayNames.add(votiOriginali.get(i));
+                    }
+                }
+                Log.i("OK", constraint.toString());
+                if (constraint.toString().equals("tutte le materie")){
+                    results.values = votiOriginali;
+                    results.count = votiOriginali.size();
+                } else {
+                    results.values = FilteredArrayNames;
+                    results.count = FilteredArrayNames.size();
+                }
+                return results;
+            }
+        };
+
+        return filter;
     }
 }
--- app/src/main/java/edu/mysobrero/FragmentVoti.java
+++ app/src/main/java/edu/mysobrero/FragmentVoti.java
@@ -5,15 +5,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FragmentVoti extends Fragment implements AdapterVoto.EventListener {
     REAPIResponse response;
     ListView listView;
+    AutoCompleteTextView listaMaterie;
+    AdapterVoto adapter;
+    List<REAPIResponse.Voti> voti;
+    List<String> materie;
+    List<Float> votiFloat;
+    TextView mediaView;
+    LineChart chart;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_voti, container, false);
@@ -27,13 +49,70 @@ public class FragmentVoti extends Fragment implements AdapterVoto.EventListener 
         listView = getView().findViewById(R.id.listaVoti);
         TextView periodo = getView().findViewById(R.id.periodoVoti);
         periodo.setText(response.user.periodo);
-        AdapterVoto adapter = new AdapterVoto(getContext(), R.layout.adapter_voti, response.voti, this);
+        chart =  getView().findViewById(R.id.graficoVoti);
+        listaMaterie = getView().findViewById(R.id.listaMaterie);
+        mediaView = getView().findViewById(R.id.mediaVoti);
+        materie = new ArrayList<> ();
+        voti = new ArrayList<>();
+        materie.add("Tutte le materie");
+        for (REAPIResponse.Voti voto: response.voti){
+            String materia = voto.materia;
+            if (!materie.contains(materia)) materie.add(materia);
+        }
+        //mediaView.setText("Media attuale:" + calcolaMedia(response.voti));
+        listaMaterie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                FragmentVoti.this.adapter.getFilter().filter(materie.get(position));
+            }
+        });
+        voti = response.voti;
+        votiFloat = new ArrayList<>();
+        adapter = new AdapterVoto(getContext(), R.layout.adapter_voti, voti, this);
+        ArrayAdapter<String> matAdapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.dropdown_item,
+                materie
+        );
+        listaMaterie.setAdapter(matAdapter);
         listView.setAdapter(adapter);
+        FragmentVoti.this.mediaView.setText("Media attuale: " + calcolaMedia(response.voti));
     }
 
-    public void onEvent() {
-        listView.invalidate();
-        Log.i("LW", "ONLAYOUT");
+    String calcolaMedia(List<REAPIResponse.Voti> listaVoti){
+        votiFloat.clear();
+        float media = 0;
+        for (REAPIResponse.Voti votoA: listaVoti){
+            float votoF = Float.parseFloat(votoA.voto.replace(",", "."));
+            media += votoF;
+            votiFloat.add(votoF);
+        }
+        Log.i("MEDIA", String.format("DIO %f", media));
+        media /= listaVoti.size();
+        DecimalFormat decimalFormat = new DecimalFormat("#,00");
+        String numberAsString = decimalFormat.format(media);
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0; i < votiFloat.size(); i++) {
+            entries.add(new Entry(i, votiFloat.get(i)));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "");
+        chart.getAxisRight().setEnabled(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        chart.getDescription().setEnabled(false);
+        dataSet.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        dataSet.setCircleColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        dataSet.setLineWidth(3);
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+        return numberAsString;
+
+    }
+
+    public void onEvent(List<REAPIResponse.Voti> votiUpdated) {
+        Log.i("REAPI", "Voti filtrati ricevuti");
+        FragmentVoti.this.mediaView.setText("Media attuale: " + calcolaMedia(votiUpdated));
     }
 }
