1464b9717f97d2225406ddd9e03e4534ac2bc78d
Federico Runco <federico.runco@gmail.com>
2019-12-26 23:20:07 +0100

    Implementato login (con relative classi reAPI)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'reapi.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -9,34 +16,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent, // transparent status bar
+          systemNavigationBarColor: Colors.black, // navigation bar color
+          statusBarIconBrightness: Brightness.dark, // status bar icons' color
+          systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icons' color
+        ),
+        child: Scaffold(
+          body: MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,6 +43,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  TextField username;
+  TextField password;
+
+  Future<http.Response> requestMethod() async {
+    var username = "3845";
+    var password = "--password--";
+    var url = "https://reapistaging.altervista.org/api.php?uname=$username&password=$password";
+    Map<String,String> headers = {
+      'Accept': 'application/json',
+    };
+    final responseHTTP = await http.get(url, headers: headers);
+    Map responseMap = jsonDecode(responseHTTP.body);
+    var response = reAPI.fromJson(responseMap);
+    if (response.status.code == 0){
+      print(response.user.nome);
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text("Errore durante il login"),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(7.0))),
+            content: new Text(response.status.description),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new OutlineButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+    return responseHTTP;
+  }
+
+
+
+  void buttonLogin(){
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+
+    requestMethod();
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,44 +106,39 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               'Accedi a mySobrero',
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'ID Studente'
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22
               ),
             ),
             TextField(
               decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Password'
+                  border: OutlineInputBorder(),
+                  labelText: 'ID Studente'
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+
+            ),
+            RaisedButton(
+              onPressed: buttonLogin,
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: const Text(
+                  'ACCEDI',
               ),
             ),
 
