80983bb170b378fb6fe7278f892f50a6b5a2dc57
Federico Runco <federico.runco@gmail.com>
2019-12-28 19:11:28 +0100

    Inizio schermata voti

--- lib/voti.dart
+++ lib/voti.dart
@@ -15,6 +15,47 @@ class _VotiView extends State<VotiView> {
   _VotiView(List<Voti> voti){
     this.voti = voti;
   }
+
+  List<Widget> generaVoti(){
+    /*return Column(
+      children: <Widget>[
+        Text("Prova")
+      ],
+    );*/
+    List<Widget> list = new List<Widget>();
+    for(var i = 0; i < voti.length; i++){
+      list.add(
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              decoration: new BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(11)),
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.topRight,
+                    end: FractionalOffset.bottomRight,
+                    colors: <Color>[Color(0xFF38f9d7), Color(0xFF43e97b)],
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text(voti[i].voto, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black)),
+                    ),
+                    Expanded(
+                        child: Text(voti[i].materia, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black) ))
+                  ],
+                ),
+              ),
+            ),
+          )
+      );
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -33,6 +74,7 @@ class _VotiView extends State<VotiView> {
                       fontSize: 24,
                     ),
                   ),
+                  Column(children: generaVoti())
                 ],
               ),
             ),
