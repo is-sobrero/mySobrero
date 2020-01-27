fea95c4dd2daad1275aaafde4139210b147e78ba
Federico Runco <federico.runco@gmail.com>
2020-01-27 17:35:11 +0100

    Bug fix

--- lib/mainview.dart
+++ lib/mainview.dart
@@ -8,9 +8,6 @@ import 'SobreroFeed.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:intl/intl.dart';
 import 'skeleton.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'globals.dart' as globals;
 
 typedef SwitchPageCallback = void Function(int page);
